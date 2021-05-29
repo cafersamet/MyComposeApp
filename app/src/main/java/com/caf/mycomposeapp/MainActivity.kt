9fd79edb9b22a5f7474c40d178b7226c9ae9432a
Cafer Samet Güllüce <cafersamet25@gmail.com>
2021-05-29 22:20:11 +0300

    Image Composable

--- app/src/main/java/com/caf/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/caf/mycomposeapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.caf.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Surface
@@ -11,6 +12,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,6 +47,28 @@ fun MainScreen() {
                 Text(text = "Button")
                 Text(text = "Test")
             }
+            Spacer(modifier = Modifier.padding(10.dp))
+            Image(
+                bitmap = ImageBitmap.imageResource(id = R.drawable.rdho),
+                "Ronaldinho, old football player",
+                modifier = Modifier
+                    .width(300.dp),
+                contentScale = ContentScale.Fit,
+                alignment = Alignment.TopCenter
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.ic_launcher_background),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Image(
+                painter = ColorPainter(Color.Blue),
+                contentDescription = null,
+                modifier = Modifier.size(16.dp, 16.dp)
+            )
         }
     }
 }
