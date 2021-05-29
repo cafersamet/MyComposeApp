a9cc81d53b5dda2daaa6c451e5125c3f70080ed7
Cafer Samet Güllüce <cafersamet25@gmail.com>
2021-05-29 15:29:29 +0300

    Modifier

--- app/src/main/java/com/caf/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/caf/mycomposeapp/MainActivity.kt
@@ -3,14 +3,20 @@ package com.caf.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
@@ -27,7 +33,14 @@ fun MainScreen(){
     Column() {
         Greeting(name = "Android")
         Greeting(name = "Compose")
-        Row(){
+        Row(modifier =
+        Modifier
+            .background(color = Color.Black)
+            .padding(12.dp)
+            .alpha(.34f)
+            .clickable {
+                println("Clicked")
+            }){
             Greeting(name = "Left")
             Greeting(name = "Right")
         }
@@ -43,7 +56,7 @@ fun MainScreen(){
 fun Greeting(name: String) {
     Text(
         text = "Hello $name!",
-        color = Color.Magenta,
+        color = Color.DarkGray,
         fontSize = 25.sp,
         fontWeight = FontWeight.Bold
     )
