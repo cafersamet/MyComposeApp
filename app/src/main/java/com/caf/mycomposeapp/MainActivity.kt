f93fe795b34fc7261baab02988975db1daeabc38
Cafer Samet Güllüce <cafersamet25@gmail.com>
2021-05-29 15:17:49 +0300

    Row,Column,Box

--- app/src/main/java/com/caf/mycomposeapp/MainActivity.kt
+++ app/src/main/java/com/caf/mycomposeapp/MainActivity.kt
@@ -3,57 +3,54 @@ package com.caf.mycomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.caf.mycomposeapp.ui.theme.MyComposeAppTheme
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
-            }
-        }
-
-        testFunction(5){
-            callbackFunc()
+            MainScreen()
         }
     }
+}
 
-    private fun testFunction(int: Int, callback: () -> Int){
-        if(int > 5){
-            callback.invoke()
-            println("My Number int > 5")
-            return;
+@Composable
+fun MainScreen(){
+    Column() {
+        Greeting(name = "Android")
+        Greeting(name = "Compose")
+        Row(){
+            Greeting(name = "Left")
+            Greeting(name = "Right")
         }
-        callback.invoke();
-        println("My Number int <= 5")
-    }
-    private fun callbackFunc(): Int {
-        println("Test")
-        return 3
+        Box(){
+            // Like stack
+            Greeting(name = "Left")
+            Greeting(name = "Right")
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(
+        text = "Hello $name!",
+        color = Color.Magenta,
+        fontSize = 25.sp,
+        fontWeight = FontWeight.Bold
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MyComposeAppTheme {
-        // A surface container using the 'background' color from the theme
-        Surface(color = MaterialTheme.colors.background) {
-            Greeting("Android")
-        }
-    }
+    MainScreen()
 }
